6e0a949cb89723fb8dfd063c33f62968e4a72067
Jason Mask <jason_mask@manulife.ca>
2020-04-18 17:52:28 -0400

    Module 4 Update

--- src/app.js
+++ src/app.js
@@ -3,21 +3,12 @@ const path = require('path');
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+const { accounts, users, writeJSON } = require('./data.js');
 
 app.set('views', path.join(__dirname + '/views'));
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/public'));
-
-let accountData = fs.readFileSync(path.join(__dirname, "json", "accounts.json"), 'utf8')
-let accounts = JSON.parse(accountData);
-
-let userData = fs.readFileSync(path.join(__dirname, "json", "users.json"), 'utf8')
-let users = JSON.parse(userData);
-
-let savings = accounts.savings;
-let checking = accounts.checking;
-let credit = accounts.credit;
 
 app.get("/", (req, res) => res.render("index", { title: "Account Summary", accounts: accounts }));
 app.get("/savings", (req, res) => res.render("account", { title: savings.nickname, account: savings }));
@@ -29,8 +20,7 @@ app.get("/transfer", (req, res) =>  res.render("transfer", { title: "Transfer", 
 app.post('/transfer', (req, res) => {
   accounts[req.body.from].balance -= req.body.amount;
   accounts[req.body.to].balance += parseInt(req.body.amount, 10);
-  let accountsJSON = JSON.stringify(accounts, null, 4)
-  fs.writeFileSync(path.join(__dirname, 'json','accounts.json'), accountsJSON, 'utf8');
+  writeJSON();
   res.render('transfer', {message: 'Transfer Completed'});
 });
 
@@ -38,8 +28,7 @@ app.get('/payment', (req, res) => res.render('payment', {account: accounts.credi
 app.post('/payment', (req, res) => {
   accounts.credit.balance -= req.body.amount;
   accounts.credit.available += parseInt(req.body.amount);
-  let accountsJSON = JSON.stringify(accounts, null, 4)
-  fs.writeFileSync(path.join(__dirname, 'json','accounts.json'), accountsJSON, 'utf8');
+  writeJSON();
   res.render('payment', {message: 'Payment Successful', account: accounts.credit});
 });
 

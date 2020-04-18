6ec1976c1ab67dbc4e3196ae66393e0c72ff2cf7
Jason Mask <jason_mask@manulife.ca>
2020-04-18 16:45:19 -0400

    Module 3 Update

--- src/app.js
+++ src/app.js
@@ -8,7 +8,21 @@ app.set('views', path.join(__dirname + '/views'));
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 
-app.get("/", (req, res) => res.render("index", { title: "Index"}));
+let accountData = fs.readFileSync(path.join(__dirname, "json", "accounts.json"), 'utf8')
+let accounts = JSON.parse(accountData);
+
+let userData = fs.readFileSync(path.join(__dirname, "json", "users.json"), 'utf8')
+let users = JSON.parse(userData);
+
+let savings = accounts.savings;
+let checking = accounts.checking;
+let credit = accounts.credit;
+
+app.get("/", (req, res) => res.render("index", { title: "Account Summary", accounts: accounts }));
+app.get("/savings", (req, res) => res.render("account", { title: savings.nickname, account: savings }));
+app.get("/checking", (req, res) => res.render("account", { title: checking.nickname, account: checking }));
+app.get("/credit", (req, res) => res.render("account", { title: credit.nickname, account: credit }));
+app.get("/profile", (req, res) => res.render("profile", { title: "Profile", user: users[0] }));
 
 app.server = app.listen(port, () => {
   console.log(`Running on port ${port}`);
